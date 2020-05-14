52b62b9a19b942f5dcaed2c992f29e0c5f149919
Axel <alejandro.perezma@e-campus.uab.cat>
2020-05-14 11:59:46 +0200

    Pruebas

--- Test_Realizar_pago_viaje.py
+++ Test_Realizar_pago_viaje.py
@@ -1,5 +1,8 @@
 import unittest
+import Bank
 
+
+# [V1]Realizar el pago de un viaje (sin considerar errores y sin necesidad de seleccionar el método de pago)
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
