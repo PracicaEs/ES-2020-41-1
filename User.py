ff194230966de43f51426a6924d47a40be3f4ee2
Axel <alejandro.perezma@e-campus.uab.cat>
2020-05-14 12:16:14 +0200

    Añadido user

--- User.py
+++ User.py
@@ -3,6 +3,6 @@ class User:
     def __init__(self):
         self.nom = ""
         self.dni = ""
-        self.dir_postal
-        self.telefono
+        self.dir_postal = None
+        self.telf = None
         self.mail = ""
