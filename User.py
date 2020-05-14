781fc2697bb4e4b69761c44ea49c34fea9098054
Axel <alejandro.perezma@e-campus.uab.cat>
2020-05-14 13:04:10 +0200

    modificado test_realizar_pago_viaje.py

--- User.py
+++ User.py
@@ -2,7 +2,7 @@ class User:
 
     def __init__(self, nom, dni, dir_postal, telf, mail):
         self.nom = nom
-        self.dni = ""
-        self.dir_postal = telf
-        self.telf = None
-        self.mail = ""
+        self.dni = dni
+        self.dir_postal = dir_postal
+        self.telf = telf
+        self.mail = mail
--- test_realizar_pago_viaje.py
+++ test_realizar_pago_viaje.py
@@ -1,14 +1,15 @@
 import unittest
-from Bank import*
-from User import*
+import Bank
+import User
+import PaymentData
 
 # [V1]Realizar el pago de un viaje (sin considerar errores y sin necesidad de seleccionar el método de pago)
 
 class MyTestCase(unittest.TestCase):
     def test_realizar_pago_viaje(self):
         banco = Bank()
-        usuario = User()
-
+        usuario = User("Agapito Pelaez Sanchez", "123456789A", 123456789, 123456789, "aaaaaaaaaa@aaaaaaaaaaa.aaa")
+        datos_pago = PaymentData()
 
         res = banco.do_payment(usuario, datos_pago)
         self.assertEqual(True, res)
