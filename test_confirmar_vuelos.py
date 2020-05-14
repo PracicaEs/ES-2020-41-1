3d8aa9018d4a22151f94902b357e40d7ac3f5443
Guillermo <guillermotesifon.faura@e-campus.uab.cat>
2020-05-14 13:31:37 +0200

    declaración test_confirmar_vuelos

---
+++ test_confirmar_vuelos.py
@@ -0,0 +1,19 @@
+import unittest
+from Skyscanner import*
+
+
+#[V1] Confirmar la reserva de los vuelos seleccionados (sin considerar errores)
+
+class TestCase(unittest.TestCase):
+    def setupClass(self):
+        self.confirm = Skyscanner()
+    def test_confirm_reserve(self):
+        user = User
+        flight = Flights
+        respuesta = self.confirm.confirm_reserve(user, flight)
+
+        self.assertEqual(respuesta, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
