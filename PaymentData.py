f3beb2f96b3fce2ac64af7fa97f8a5aa7434dfce
Axel <alejandro.perezma@e-campus.uab.cat>
2020-05-17 15:15:49 +0200

    modificado test_realizar_pago_viaje.py modificado test error pago modificadas funciones de bank

--- PaymentData.py
+++ PaymentData.py
@@ -1,7 +1,7 @@
 class PaymentData:
 
-    def __init__(self):
-        self.tipo_tarjeta = ""
-        self.nom_titular = ""
-        self.codigo_seg = None
-        self.importe = None
+    def __init__(self, tipo_tarjeta, nom_titular, codigo_seg, importe):
+        self.tipo_tarjeta = tipo_tarjeta
+        self.nom_titular = nom_titular
+        self.codigo_seg = codigo_seg
+        self.importe = importe
--- test_realizar_pago_viaje.py
+++ test_realizar_pago_viaje.py
@@ -3,19 +3,14 @@ from Bank import *
 from User import *
 from PaymentData import *
 
-# [V1]Realizar el pago de un viaje (sin considerar errores y sin necesidad de seleccionar el método de pago)
-
 
 class MyTestCase(unittest.TestCase):
     def test_do_payment(self):
-
         bank = Bank()
         user = User("Agapito Pelaez Sanchez", "123456789A", 123456789, 123456789, "aaa@aaa.aaa")
-        payment_data = PaymentData()
+        payment_data = PaymentData("Visa", "Agapito Pelaez Sanchez", 123, 999)
         res = bank.do_payment(user, payment_data)
         self.assertEqual(True, res)
-
-
 
     def test_error_payment(self):
         def do_payment(a: User, b: PaymentData):
@@ -25,10 +20,9 @@ class MyTestCase(unittest.TestCase):
         bank = Bank()
         bank.do_payment = do_payment
         user = User("Agapito Pelaez Sanchez", "123456789A", 123456789, 123456789, "aaa@aaa.aaa")
-        payment_data = PaymentData()
+        payment_data = PaymentData("Visa", "Agapito Pelaez Sanchez", 123, 999)
         res = bank.do_payment(user, payment_data)
         self.assertEqual(False, res)
-
 
 
 if __name__ == '__main__':
