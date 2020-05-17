2a55030dbb0275d0318b39228b166d2c616defce
Axel <alejandro.perezma@e-campus.uab.cat>
2020-05-17 15:07:55 +0200

    modificado test_realizar_pago_viaje.py añadido test error pago

--- test_realizar_pago_viaje.py
+++ test_realizar_pago_viaje.py
@@ -9,18 +9,25 @@ from PaymentData import *
 class MyTestCase(unittest.TestCase):
     def test_do_payment(self):
 
+        bank = Bank()
+        user = User("Agapito Pelaez Sanchez", "123456789A", 123456789, 123456789, "aaa@aaa.aaa")
+        payment_data = PaymentData()
+        res = bank.do_payment(user, payment_data)
+        self.assertEqual(True, res)
+        print("Pago realizado con éxito")
+
+
+    def test_error_payment(self):
         def do_payment(a: User, b: PaymentData):
             return False
 
         bank = Bank()
         bank.do_payment = do_payment
-
-        user = User("Agapito Pelaez Sanchez", "123456789A", 123456789, 123456789, "aaaaaaaaaa@aaaaaaaaaaa.aaa")
+        user = User("Agapito Pelaez Sanchez", "123456789A", 123456789, 123456789, "aaa@aaa.aaa")
         payment_data = PaymentData()
-
         res = bank.do_payment(user, payment_data)
-
         self.assertEqual(False, res)
+        print("Error al realizar el pago")
 
 
 if __name__ == '__main__':
