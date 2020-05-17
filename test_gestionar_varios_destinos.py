568fb93f4bd66d57e2e3c121284193127d0b62ab
Arman <arman.pipoyan26@gmail.com>
2020-05-17 14:13:56 +0200

    test_gestionar_varios_destinos.py: Test viejo quitado

--- test_gestionar_varios_destinos.py
+++ test_gestionar_varios_destinos.py
@@ -3,22 +3,6 @@ from Travel import*
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        n = 3
-        x = 3
-        flight1 = Flight("1", "A", n, 200.0)
-        flight2 = Flight("1", "B", n, 200.0)
-        flight3 = Flight("1", "C", n, 200.0)
-        s1 = flight1.get_destination()
-        s2 = flight2.get_destination()
-        s3 = flight3.get_destination()
-        destinations = list({s1, s2, s3})
-        destinations_list = Skyscanner(destinations, n)
-        num_destinations = destinations_list.get_num_destinations()
-        travels = destinations_list.get_destinations()
-        self.assertEqual(n, num_destinations)
-        self.assertEqual(destinations, travels)
-
     def test_no_destinations(self):
         passengers = 2
         destinations = []
