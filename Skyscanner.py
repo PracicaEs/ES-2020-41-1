74fd60c445e378fdac225138e3725e3acbc457d4
jordi <jordi.domingoc@e-campus.uab.cat>
2020-05-17 12:31:47 +0200

    Gestionar varios destinos V1

--- Skyscanner.py
+++ Skyscanner.py
@@ -3,9 +3,15 @@ from Flights import *
 
 
 class Skyscanner():
-    def __init__(self, x):
-        self.num_destinos = x
-        self.Destinos = []
+    def __init__(self, destination: list, num_destination: int):
+        self.num_destination = num_destination
+        self.destination = destination
+
+    def get_num_destinnations(self) -> int:
+        return self.num_destination
+
+    def get_destinnations(self) -> list:
+        return self.destination
 
     def confirm_reserve(self, user: User, flights: Flights) -> bool:
         return True
