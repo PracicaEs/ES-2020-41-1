e41421fb7a807a17c8bf7a63aa2c7dff6339aef4
Arman <arman.pipoyan26@gmail.com>
2020-05-17 15:41:31 +0200

    Getters de Travel.py modificados

--- Flights.py
+++ Flights.py
@@ -4,3 +4,4 @@ from Flight import *
 class Flights:
     def __init__(self, flights: list):
         self.flights = flights
+
--- Travel.py
+++ Travel.py
@@ -6,7 +6,7 @@ class Travel:
     def __init__(self, destinations: list, user: User, passengers: int):
         self.destinations = destinations
         self.passengers = passengers
-        self.flights = []
+        self.flights = Flights([])
         self.assign_flights()
         self.total_price = 0.0
         self.user = user
@@ -17,16 +17,19 @@ class Travel:
     def get_passengers(self) -> int:
         return self.passengers
 
-    def get_flights(self) -> list:
+    def get_flights(self) -> Flights:
         return self.flights
+
+    def get_flights_list(self) -> list:
+        return [f for f in self.flights.flights]
 
     def get_total_price(self) -> float:
         return self.total_price
 
     def assign_flights(self) -> None:
-        for i in range(len(self.flights), len(self.destinations)):
+        for i in range(len(self.flights.flights), len(self.destinations)):
             f1 = Flight(str(i), self.destinations[i], self.passengers, i+1)
-            self.flights.append(f1)
+            self.flights.flights.append(f1)
 
     def add_destination(self, destination: str) -> None:
         self.destinations.append(destination)
@@ -35,13 +38,13 @@ class Travel:
 
     def remove_destination(self, destination: str) -> None:
         self.destinations.remove(destination)
-        for i, flight in enumerate(self.flights):
+        for i, flight in enumerate(self.flights.flights):
             if flight.get_destination() == destination:
-                self.flights.pop(i)
+                self.flights.flights.pop(i)
         self.recalculate_price()
 
     def recalculate_price(self) -> None:
         price = 0.0
-        for flight in self.flights:
+        for flight in self.flights.flights:
             price += flight.get_price() * flight.get_passengers()
         self.total_price = price
